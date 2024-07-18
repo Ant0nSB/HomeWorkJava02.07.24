0071fb1bd55a1dd8ad185aa705755a1a3ee72da3
Ant0nSB <Jackpot.8.8.8.8.8A@gmail.com>
2024-07-18 19:40:00 +0200

    HomeWorkJava020724

---
+++ src/HomeWork.java
@@ -0,0 +1,19 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        char   c = 'G';
+        int    i = 89;
+        byte   b = 4;
+        short  s = 56;
+        float  f = 4.7333436f;
+        double d = 4.355453532;
+        long   l = 12121;
+        System.out.println("Character:"+c);
+        System.out.println("Integer:"+i);
+        System.out.println("Byte:"+b);
+        System.out.println("Short:"+s);
+        System.out.println("Float:"+f);
+        System.out.println("Double:"+d);
+        System.out.println("Long:"+l);
+
+    }
+}
